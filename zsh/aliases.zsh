e6cebbf05b7f486617cfd0f64e17e83f035425b5
Jan Šibil <21-1@unicornuniverse.eu>
2014-05-29 13:14:22 +0200

    Add alias for 'sudo mtr'

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -154,3 +154,6 @@ alias srdmt='spring rake db:migrate db:test:prepare'
 alias sp='sprintly'
 # spb = sprintly branch - create a branch automatically based on the bug you're working on
 alias spb="git checkout -b \`sp | tail -2 | grep '#' | sed 's/^ //' | sed 's/[^A-Za-z0-9 ]//g' | sed 's/ /-/g' | cut -d"-" -f1,2,3,4,5\`"
+
+# MTR
+alias smtr='sudo mtr'
